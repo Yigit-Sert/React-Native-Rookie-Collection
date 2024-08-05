09aa956e7d5b766df5c81e60ac6a3272567ba203
Yigit-Sert <yigitsert32@gmail.com>
2024-08-05 15:34:34 +0300

    chore: Update project structure and remove unused code

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,41 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  ActivityIndicator,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import ProjectListItem from "../components/core/ProjectListItem";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import {
-  AmaticSC_400Regular,
-  AmaticSC_700Bold,
-} from "@expo-google-fonts/amatic-sc";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-
-SplashScreen.preventAutoHideAsync();
+import { FlatList, StyleSheet, View } from "react-native";
+import ProjectListItem from "@/components/core/ProjectListItem";
 
 const projects = [...Array(30)].map((val, index) => index + 1);
 
 export default function HomeScreen() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold,
-  });
-
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-  
   return (
     <View style={styles.container}>
       <FlatList
