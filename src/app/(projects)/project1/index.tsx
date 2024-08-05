3432496b9bbaa9ed675aa7806d3461048e718da8
Yigit-Sert <yigitsert32@gmail.com>
2024-08-05 15:43:00 +0300

    feat: Add readme screen to project1 app to refactor later on

--- src/app/(projects)/project1/index.tsx
+++ src/app/(projects)/project1/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+
+// Remove this style import when a ui library is applied
+import { StyleSheet } from "react-native";
 
 const App = () => {
-    return (
-        <View>
-            <Text>Hello World!</Text>
-        </View>
-    );
+  return (
+    <View>
+      <Stack.Screen options={{ title: "Readme of the application" }} />
+      <Text style={styles.text}>
+        this page is supposed to be the readme in the app
+      </Text>
+    </View>
+  );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 20,
+  },
+});
